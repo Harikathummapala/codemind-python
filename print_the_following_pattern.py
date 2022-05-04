78c974ca62fbf6319ae37e77b849ca2259570f02
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-05-04 21:34:51 +0530

    n=int(input()) a=n for i in range(1,n+1):     for j in range(1,n+1):         print(a,end=' ')         a=a-1     a=n     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,8 @@
 n=int(input())
+a=n
 for i in range(1,n+1):
-    for   j in  range(1,n+1):
-        print(chr(i+64),end=' ')
+    for j in range(1,n+1):
+        print(a,end=' ')
+        a=a-1
+    a=n
     print()
