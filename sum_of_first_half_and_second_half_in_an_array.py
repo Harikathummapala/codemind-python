37cbea6d2c1d433b5f21336039fda7340e8e3a28
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-06-27 21:48:33 +0530

    x=int(input()) a=list(map(int,input().strip().split())) k=x//2 sum=0 sum1=0 for i in range( 0,k):     sum=sum+a[i] for i in range(k,x):     sum1=sum1+a[i] print(sum) print(sum1)

---
+++ sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,12 @@
+x=int(input())
+a=list(map(int,input().strip().split()))
+k=x//2
+sum=0
+sum1=0
+for i in range( 0,k):
+    sum=sum+a[i]
+for i in range(k,x):
+    sum1=sum1+a[i]
+print(sum)
+print(sum1)
+    
