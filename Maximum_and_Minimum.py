811bc355612d9c15bec0da0b0bbe19959391b5e0
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-06-30 20:30:32 +0530

    n=int(input()) a=list(map(int,input().split())) k=[] sum=0 c=0 for i in range(n):     if a[i]!=-1:         c=1         for j in range(n):             if a[i]==a[j] and i!=j:                 c=c+1                 a[j]=-1         if a[i]==c:             k.append(a[i]) if len(k)==0:     print(-1) else:     print(min(k),max(k))

---
+++ Maximum_and_Minimum.py
@@ -0,0 +1,18 @@
+n=int(input())
+a=list(map(int,input().split()))
+k=[]
+sum=0
+c=0
+for i in range(n):
+    if a[i]!=-1:
+        c=1
+        for j in range(n):
+            if a[i]==a[j] and i!=j:
+                c=c+1
+                a[j]=-1
+        if a[i]==c:
+            k.append(a[i])
+if len(k)==0:
+    print(-1)
+else:
+    print(min(k),max(k))
