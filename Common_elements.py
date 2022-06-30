28301754ce50ad22ade65eff1bc8b7a9f0119485
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-06-30 10:32:36 +0530

    a,b=map(int,input().split()) arr=list(map(int,input().split())) arr2=list(map(int,input().split())) c=0 for i in range(a):     for j in range(a):         if i!=j:             if arr[i]==arr[j] and arr[i]!=-1:                 arr[j]=-1 for i in range(b):     for j in range(b):         if i!=j:             if arr2[i]==arr2[j] and arr2[i]!=-1:                 arr2[j]=-1 for i in arr:     if i!=-1:         if i in arr2:             print(i,end=" ")

--- Common_elements.py
+++ Common_elements.py
@@ -1,8 +1,18 @@
-n,m= map(int,input().split())
-a1=list(map(int,input().split()))
-a2=list(map(int,input().split()))
-p=[]
-for i in a1:
-    if i in a2 and i not in p:
-        p.append(i)
-print(*p)
+a,b=map(int,input().split())
+arr=list(map(int,input().split()))
+arr2=list(map(int,input().split()))
+c=0
+for i in range(a):
+    for j in range(a):
+        if i!=j:
+            if arr[i]==arr[j] and arr[i]!=-1:
+                arr[j]=-1
+for i in range(b):
+    for j in range(b):
+        if i!=j:
+            if arr2[i]==arr2[j] and arr2[i]!=-1:
+                arr2[j]=-1
+for i in arr:
+    if i!=-1:
+        if i in arr2:
+            print(i,end=" ")
