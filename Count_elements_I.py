c3d9b938ccfe219b3731446d91b74bd685eee27b
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-06-30 18:20:50 +0530

    a,b=map(int,input().split()) l=list(map(int,input().split())) m=list(map(int,input().split())) k=[] for i in range(a):     if l[i] in m:         k.append(l[i]) print(len(set(k)))

--- Count_elements_I.py
+++ Count_elements_I.py
@@ -1,9 +1,8 @@
-n,m= map(int,input().split())
-a1=set(map(int,input().split()))
-a2=set(map(int,input().split()))
-p=[]
-c=list(a1)+list(a2)
-for i in range(len(c)):
-    if i  not in p and c.count(i)>1:
-        p.append(i)
-print(len(p))
+a,b=map(int,input().split())
+l=list(map(int,input().split()))
+m=list(map(int,input().split()))
+k=[]
+for i in range(a):
+    if l[i] in m:
+        k.append(l[i])
+print(len(set(k)))
