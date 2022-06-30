23427c6eb68f94b388c831e52eaec55c8399376a
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-06-30 11:03:55 +0530

    n,k=map(int,input().split()) c=0 a=list(map(int,input().split())) for i in range(n):     if a[i]%k!=0:         c=c+1 print(c)

--- Count_elements_not_divisible_by_K.py
+++ Count_elements_not_divisible_by_K.py
@@ -1,9 +1,7 @@
-x,k= map(int,input().split())
-a=list(map(int,input().split()))
+n,k=map(int,input().split())
 c=0
-z=[]
-for i in range(x):
+a=list(map(int,input().split()))
+for i in range(n):
     if a[i]%k!=0:
         c=c+1
-   
 print(c)
