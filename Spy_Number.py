c2daeb703c48e285b653861ea089c808a65937ea
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-06-30 21:52:13 +0530

    a=int(input()) s=0 p=1 for i in str(a):     s=s+int(i)     p=p*int(i) if s==p:     print("Spy Number") else:     print("Not Spy Number")

--- Spy_Number.py
+++ Spy_Number.py
@@ -1,12 +1,10 @@
-n=int(input())
-sum=0
+a=int(input())
+s=0
 p=1
-while n!=0:
-    r=n%10
-    sum=sum+r
-    p=p*r
-    n=n//10
-if sum==p:
+for i in str(a):
+    s=s+int(i)
+    p=p*int(i)
+if s==p:
     print("Spy Number")
 else:
     print("Not Spy Number")
