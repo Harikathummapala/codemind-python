b0553212873a91e37ec6778b2ca3a6f3ecf465d6
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-10-11 21:01:28 +0530

    n=int(input()) t=n t2=n s=0 su=0 while t>0:     r=t%10     s+=1     t//=10 while t2>0:     r=t2%10     su+=pow(r,s)     s-=1     t2//=10 if su==n:     print("True") else:     print("False")

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,11 +1,19 @@
-a=int(input())
-sum=0
-b=1
-
-for i in str(a):
-    sum=sum+int(i)**b
-    b=b+1
-if(a==sum):
+n=int(input())
+t=n
+t2=n
+s=0
+su=0
+while t>0:
+    r=t%10
+    s+=1
+    t//=10
+while t2>0:
+    r=t2%10
+    su+=pow(r,s)
+    s-=1
+    t2//=10
+if su==n:
     print("True")
 else:
     print("False")
+    
