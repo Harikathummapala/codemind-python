d9ae0ead9a76ae01a823fb6f7b39d31055df1008
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-10-12 14:49:30 +0530

    a,b=map(int,input().split()) arr=[] for i in range(a):     l=list(map(int,input().split()))     arr.append(sum(l)) print(*arr)

--- Row_wise_sum_of_a_matrix.py
+++ Row_wise_sum_of_a_matrix.py
@@ -1,13 +1,6 @@
-m,n=map(int,input().split())
-a=[]
-s=0
-max=0
-for i in range(m):
+a,b=map(int,input().split())
+arr=[]
+for i in range(a):
     l=list(map(int,input().split()))
-    if len(l)==n:
-        a.append(l)
-for i in range(m):
-    for j in range(n):
-        s=s+a[i][j]
-    print(s,end=" ")
-    s=0
+    arr.append(sum(l))
+print(*arr)
