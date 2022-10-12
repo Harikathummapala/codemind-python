f5c09fffaf09df9ed1d1be5b8123910314cc8099
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-10-12 12:13:07 +0530

    a,b=map(int,input().split()) arr=[] for i in range(a):     l=list(map(int,input().split()))     arr.append(sum(l)) print(sum(arr))

--- Sum_of_all_elements_in_a_matrix.py
+++ Sum_of_all_elements_in_a_matrix.py
@@ -1,11 +1,6 @@
-m,n=map(int,input().split())
-a=[]
-s=0
-for i in range(m):
+a,b=map(int,input().split())
+arr=[]
+for i in range(a):
     l=list(map(int,input().split()))
-    if len(l)==n:
-        a.append(l)
-for i in range(m):
-    for j in range(n):
-        s=s+a[i][j]
-print(s)
+    arr.append(sum(l))
+print(sum(arr))
