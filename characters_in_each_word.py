5273c4077b102738f47240abf51a42d3abd1d322
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-10-13 20:55:35 +0530

    l=list(map(str,input().split())) for i in l:     print(len(i),end=' ')

--- characters_in_each_word.py
+++ characters_in_each_word.py
@@ -1,10 +1,3 @@
-n=input()
-c=0
-for i in n:
-    if ord(i)!=32:
-        c=c+1
-    if ord(i)==32:
-        print(c,end=" ")
-        c=0
-else:       
-    print(c,end=" ")
+l=list(map(str,input().split()))
+for i in l:
+    print(len(i),end=' ')
