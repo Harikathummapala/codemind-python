3e7672501c309660caae401413b29fe5dc6ad766
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-10-13 21:39:26 +0530

    s=input() s=s.lower() arr=[] for char in s:     if char.isalpha():         arr.append(char) if(len(set(arr))==26):     print(True) else:     print(False)

--- Pangram_or_not.py
+++ Pangram_or_not.py
@@ -1,12 +1,10 @@
-n=input()
-n1=n.lower()
-c=0
-s=''
-for i in n1:
-  if ord(i)>=97 and ord(i)<=122:
-    if i  not in s:
-        s=s+i
-if len(s)==26:
-    print("True")
+s=input()
+s=s.lower()
+arr=[]
+for char in s:
+    if char.isalpha():
+        arr.append(char)
+if(len(set(arr))==26):
+    print(True)
 else:
-    print("False")
+    print(False)
