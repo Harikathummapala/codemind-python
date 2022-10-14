9a0bd31647523f15ef6d30550ff7a56e6b6b0231
Harikathummapala <97904051+Harikathummapala@users.noreply.github.com>
2022-10-14 18:13:17 +0530

    s=input() c=0 for ch in s:     if  not(ch.isalpha() or ch.isdigit() or ch==' '):         c+=1 print(c)

--- special_characters_count.py
+++ special_characters_count.py
@@ -1,10 +1,6 @@
-n=input()
-s=n.upper()
-c2=0
-c1=0
-for i in s:
-    if(ord(i)>=65 and ord(i)<=90) or ord(i)==32:
-        c1=c1+1
-    else:
-        c2=c2+1
-print(c2)
+s=input()
+c=0
+for ch in s:
+    if  not(ch.isalpha() or ch.isdigit() or ch==' '):
+        c+=1
+print(c)
